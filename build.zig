4f6d4bc7722635cc6f416b15cc0bcb3837969574
LaBatata101 <labatata101@gmail.com>
2025-11-18 19:55:19 -0300

    Add custom test runner for unit tests

--- build.zig
+++ build.zig
@@ -143,59 +143,47 @@ pub fn build(b: *std.Build) void {
     const test_filters = b.option([]const []const u8, "test-filter", "Skip tests that do not match any filter") orelse &[0][]const u8{};
     const no_run = b.option(bool, "no-run", "Don't run the test") orelse false;
 
-    // Creates an executable that will run `test` blocks from the provided module.
-    // Here `mod` needs to define a target, which is why earlier we made sure to
-    // set the releative field.
-    const mod_tests = b.addTest(.{
-        .name = "mod-test",
-        .root_module = mod,
-        .filters = test_filters,
-        .use_llvm = true,
-    });
-    const mod_test_artifacts = b.addInstallArtifact(mod_tests, .{
-        .dest_dir = .{
-            .override = .{
-                .custom = "tests",
-            },
-        },
-    });
-
-    // A run step that will run the test executable.
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    // Creates an executable that will run `test` blocks from the executable's
-    // root module. Note that test executables only test one module at a time,
-    // hence why we have to create two separate ones.
-    const exe_tests = b.addTest(.{
-        .name = "exe-test",
-        .root_module = exe.root_module,
-        .filters = test_filters,
-        .use_llvm = true,
-    });
-    b.installArtifact(exe_tests);
-
-    // A run step that will run the second test executable.
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    // A top level step for running all tests. dependOn can be called multiple
-    // times and since the two run steps do not depend on one another, this will
-    // make the two of them run in parallel.
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&mod_test_artifacts.step);
-    if (!no_run) {
-        test_step.dependOn(&run_mod_tests.step);
-        test_step.dependOn(&run_exe_tests.step);
-    }
 
-    // Just like flags, top level steps are also listed in the `--help` menu.
-    //
-    // The Zig build system is entirely implemented in userland, which means
-    // that it cannot hook into private compiler APIs. All compilation work
-    // orchestrated by the build system will result in other Zig compiler
-    // subcommands being invoked with the right flags defined. You can observe
-    // these invocations when one fails (or you pass a flag to increase
-    // verbosity) to validate assumptions and diagnose problems.
-    //
-    // Lastly, the Zig build system is relatively simple and self-contained,
-    // and reading its source code will allow you to master it.
+        // Creates an executable that will run `test` blocks from the provided module.
+        // Here `mod` needs to define a target, which is why earlier we made sure to
+        // set the releative field.
+        const mod_tests = b.addTest(.{
+            .name = "ness-mod-test",
+            .root_module = mod,
+            .filters = test_filters,
+            .use_llvm = true,
+            .test_runner = .{ .path = b.path("src/test_runners/test_runner.zig"), .mode = .simple },
+        });
+        const mod_test_artifacts = b.addInstallArtifact(mod_tests, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = "tests",
+                },
+            },
+        });
+
+        // A run step that will run the test executable.
+        const run_mod_tests = b.addRunArtifact(mod_tests);
+
+        // Creates an executable that will run `test` blocks from the executable's
+        // root module. Note that test executables only test one module at a time,
+        // hence why we have to create two separate ones.
+        const exe_tests = b.addTest(.{
+            .name = "exe-test",
+            .root_module = exe.root_module,
+            .filters = test_filters,
+            .use_llvm = true,
+            .test_runner = .{ .path = b.path("src/test_runners/test_runner.zig"), .mode = .simple },
+        });
+        b.installArtifact(exe_tests);
+
+        // A run step that will run the second test executable.
+        const run_exe_tests = b.addRunArtifact(exe_tests);
+
+        test_step.dependOn(&mod_test_artifacts.step);
+        if (!no_run) {
+            test_step.dependOn(&run_mod_tests.step);
+            test_step.dependOn(&run_exe_tests.step);
+        }
 }
