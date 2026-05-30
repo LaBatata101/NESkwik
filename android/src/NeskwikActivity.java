5abce6eae41b2b9d26f47d87d81b88df60603388
LaBatata101 <labatata101@gmail.com>
2026-05-30 11:42:00 -0300

    Fix permission error on Android

--- android/src/NeskwikActivity.java
+++ android/src/NeskwikActivity.java
@@ -1,6 +1,6 @@
 package com.labatata.neskwik;
 
-import android.app.Activity;
+import android.content.ClipData;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -8,9 +8,18 @@ import android.provider.OpenableColumns;
 
 import org.libsdl.app.SDLActivity;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class NeskwikActivity extends SDLActivity {
+    private static final String TAG = "NESkwik";
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null) {
+            persistUriPermissions(data);
+        }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
@@ -34,6 +43,20 @@ public class NeskwikActivity extends SDLActivity {
         } finally {
             if (cursor != null) {
                 cursor.close();
+            }
+        }
+    }
+
+    private void persistUriPermissions(Intent data) {
+        int permissionFlags = data.getFlags() &
+            (Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+        getContentResolver().takePersistableUriPermission(data.getData(), permissionFlags);
+
+        ClipData clipData = data.getClipData();
+        if (clipData != null) {
+            for (int i = 0; i < clipData.getItemCount(); i++) {
+                getContentResolver().takePersistableUriPermission(clipData.getItemAt(i).getUri(), permissionFlags);
             }
         }
     }
